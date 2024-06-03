3b7e345fe8b9d70fd577721fbf5efa9db2bf50e1
geb <853934146@qq.com>
2024-06-03 09:18:36 +0800

    fix: fix env bug

--- src/lib/components/NavMenu.svelte
+++ src/lib/components/NavMenu.svelte
@@ -147,7 +147,7 @@
 	>
 		Settings
 	</a>
-	{#if PUBLIC_APP_NAME === "LLMsChat"}
+	{#if envPublic.PUBLIC_APP_NAME === "LLMsChat"}
 		<a
 			href="{base}/privacy"
 			class="flex h-9 flex-none items-center gap-1.5 rounded-lg pl-2.5 pr-2 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
